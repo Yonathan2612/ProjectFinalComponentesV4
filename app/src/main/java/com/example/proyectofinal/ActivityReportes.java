c92e2e81753304271cf857d10b776ab12090d777
Yonathan David Acosta <ydacostab@correo.udistrital.edu.co>
2022-04-06 20:46:40 -0500

    Modulo reportes V2

--- app/src/main/java/com/example/proyectofinal/ActivityReportes.java
+++ app/src/main/java/com/example/proyectofinal/ActivityReportes.java
@@ -3,12 +3,19 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityReportes extends AppCompatActivity {
-
+    Button btnVerReportes;
+    TextView txtFecha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportes);
+    btnVerReportes=(Button) findViewById(R.id.btnVerReporte);
+    txtFecha=(TextView) findViewById(R.id.txtDate);
+
+
     }
 }
