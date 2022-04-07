7b198aea88cf97e65c118561fdb2025b5de2e086
Edwin <89475194+EdwinVaquiro@users.noreply.github.com>
2022-04-06 23:00:52 -0500

    Mostrar Zoo COn la base de datos

--- app/src/main/java/com/example/proyectofinal/modulousuario/zoou/zooUFragment.java
+++ app/src/main/java/com/example/proyectofinal/modulousuario/zoou/zooUFragment.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,6 +19,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.proyectofinal.ActivityIngresar;
 import com.example.proyectofinal.R;
 import com.example.proyectofinal.databinding.FragmentZoouBinding;
 
@@ -24,7 +28,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class zooUFragment extends Fragment {
-    TextView nomZOO,especiesZOO,tamaZOO;
+    TextView nomZOO,especiesZOO,tamaZOO, nombrezoo;
     RequestQueue requestQueue;
     private FragmentZoouBinding binding;
 
@@ -35,11 +39,12 @@ public class zooUFragment extends Fragment {
 
             binding = FragmentZoouBinding.inflate(inflater, container, false);
             View root = binding.getRoot();
-        final Button btnBuscarZoo = (Button) root.findViewById(R.id.btnBuscarZOO);
-        nomZOO=root.findViewById(R.id.nomZoo);
-        especiesZOO=root.findViewById(R.id.especiesZOO);
-        tamaZOO=root.findViewById(R.id.tamaZOO);
-        btnBuscarZoo.setOnClickListener(this::onClick);
+            final Button btnBuscarZoo = (Button) root.findViewById(R.id.btnBuscarZOO);
+            nomZOO=(EditText)root.findViewById(R.id.nomZoo2);
+             especiesZOO=(EditText)root.findViewById(R.id.especiesZOO);
+             tamaZOO=(EditText)root.findViewById(R.id.tamaZOO);
+            nombrezoo=(EditText) root.findViewById(R.id.nomZoo);
+             btnBuscarZoo.setOnClickListener(this::onClick);
 
 
             return root;
@@ -50,40 +55,54 @@ public class zooUFragment extends Fragment {
         if(id==R.id.btnBuscarZOO)
         {
             String nombre=nomZOO.getText().toString().trim();
-            verZOO(nombre);
+            if(nombre.isEmpty())
+            {
+                Toast.makeText(getContext(), "El campo debe ser valido", Toast.LENGTH_SHORT).show();
+
+
+            }else
+            {
+                verZOO(nombre);
+            }
+
         }
     }
 
 
 
 
-    private void verZOO(String Nom){
+    private void verZOO(String Nom)
+    {
         String URL="https://compbdzoo.000webhostapp.com/mostrarZOO.php?nombre="+Nom;
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET, URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONObject response)
+                    {
 
                         String nombre, Especies,Tamano;
                         try {
-                            nombre = response.getString("nombre");
-                            Especies = response.getString("Especies");
-                            Tamano = response.getString("Tamano");
-                            nomZOO.setText(nombre);
+                            nombre = response.getString("nombreZoo");
+                            Especies = response.getString("especies");
+                            Tamano = response.getString("tamano");
+                            nombrezoo.setText(nombre);
                             especiesZOO.setText(Especies);
                             tamaZOO.setText(Tamano);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        } catch (JSONException e)
+                        {
+                            Toast.makeText(getContext(), "ERROR EN: "+e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText(getContext(), "El ZOO no fue encontrado ", Toast.LENGTH_SHORT).show();
             }
         }
         );
+        requestQueue = Volley.newRequestQueue(getContext());
         requestQueue.add(jsonObjectRequest);
     }
 
