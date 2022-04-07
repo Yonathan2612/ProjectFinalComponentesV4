8c46879019366343ceb9c72da103745cdfdb2ed4
FRANCISCO <fapenuelag@correo.udistrital.edu.co>
2022-04-07 00:07:51 -0500

    actualizacion

---
+++ app/src/main/java/com/example/proyectofinal/Modelo/CRUDanimales.java
@@ -0,0 +1,337 @@
+package com.example.proyectofinal.Modelo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.proyectofinal.ActivityRegistrar;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+public class CRUDanimales extends ActivityRegistrar
+{
+    private String nom_vulgar,nom_cientifico,txtfamilia,txtextincion,txtidentificacion,txtespecie,txtgenero,txtnacimiento,txtzoo,txtpaisor,txtcontinente;
+    public CRUDanimales(String nom_vulgar, String nom_cientifico, String txtfamilia, String txtextincion, String txtidentificacion, String txtespecie, String txtgenero, String txtnacimiento, String txtzoo, String txtpaisor, String txtcontinente)
+    {
+        this.nom_vulgar = nom_vulgar;
+        this.nom_cientifico = nom_cientifico;
+        this.txtfamilia = txtfamilia;
+        this.txtextincion = txtextincion;
+        this.txtidentificacion = txtidentificacion;
+        this.txtespecie = txtespecie;
+        this.txtgenero = txtgenero;
+        this.txtnacimiento = txtnacimiento;
+        this.txtzoo = txtzoo;
+        this.txtpaisor = txtpaisor;
+        this.txtcontinente = txtcontinente;
+    }
+    public CRUDanimales(){ }
+
+    public boolean insertar(CRUDanimales ob)
+    {
+        boolean[] estado = {false};
+
+        StringRequest request = new StringRequest(Request.Method.POST, "https://compbdzoo.000webhostapp.com/insertarAnimal.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        if(response.equalsIgnoreCase("Registro con éxito")){
+                           // Toast.makeText(ActivityRegistrar.this, "Registro no fue éxitoso", Toast.LENGTH_SHORT).show();
+                            estado[0]=false;
+                        }
+                        else{
+                            //Toast.makeText(ActivityRegistrar.this, "El registro fue exitoso por favor ingrese", Toast.LENGTH_SHORT).show();
+                            estado[0]=true;
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+
+            }
+        }
+
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("nom_vulgar",ob.getnom_vulgar());
+                params.put("nom_cientifico",ob.getnom_cientifico());
+                params.put("txtfamilia",ob.gettxtfamilia());
+                params.put("txtextincion",ob.gettxtextincion());
+                params.put("txtidentificacion",ob.gettxtidentificacion());
+                params.put("txtespecie",ob.gettxtespecie());
+                params.put("txtgenero",ob.gettxtgenero());
+                params.put("txtnacimiento",ob.gettxtnacimiento());
+                params.put("txtzoo",ob.gettxtzoo());
+                params.put("txtpaisor",ob.gettxtpaisor());
+                params.put("txtcontinente",ob.gettxtcontinente());
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+
+        return estado[0];
+    }
+
+    public  void Editar(CRUDanimales ob)
+    {
+
+    }
+
+    public boolean ComprobarExistencia()
+    {
+        return true;
+    }
+
+    public List<CRUDanimales> ListaAnimales()
+    {
+        List<CRUDanimales> lista = new List<CRUDanimales>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(@Nullable Object o) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Iterator<CRUDanimales> iterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @NonNull
+            @Override
+            public <T> T[] toArray(@NonNull T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(CRUDanimales cruDanimales) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(@Nullable Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(@NonNull Collection<? extends CRUDanimales> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, @NonNull Collection<? extends CRUDanimales> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public CRUDanimales get(int index) {
+                return null;
+            }
+
+            @Override
+            public CRUDanimales set(int index, CRUDanimales element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, CRUDanimales element) {
+
+            }
+
+            @Override
+            public CRUDanimales remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(@Nullable Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(@Nullable Object o) {
+                return 0;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<CRUDanimales> listIterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<CRUDanimales> listIterator(int index) {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public List<CRUDanimales> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+
+        return lista;
+    }
+
+    public boolean Login(String correo, String password)
+    {
+        return false;
+    }
+
+    public String TOSTRING()
+    {
+        return "nom_vulgar:"+this.getnom_vulgar()+"\n" +
+                "nom_cientifico: "+this.getnom_cientifico()+"\n" +
+                "txtfamilia: "+this.gettxtfamilia()+"\n" +
+                "txtextincion: "+this.gettxtextincion()+"\n"+
+        "txtidentificacion:"+this.gettxtidentificacion()+"\n" +
+                "txtespecie: "+this.gettxtespecie()+"\n" +
+                "txtgenero: "+this.gettxtgenero()+"\n" +
+                "txtnacimiento: "+this.gettxtnacimiento()+"\n"+
+        "txtzoo:"+this.gettxtzoo()+"\n" +
+                "txtpaisor: "+this.gettxtpaisor()+"\n" +
+                "txtcontinente: "+this.gettxtcontinente()+"\n" ;
+    }
+
+    public String getnom_vulgar() {
+        return nom_vulgar;
+    }
+
+    public void setnom_vulgar(String nombre) {
+        this.nom_vulgar = nombre;
+    }
+
+    public String getnom_cientifico() {
+        return nom_cientifico;
+    }
+
+    public void setnom_cientifico(String correo) {
+        this.nom_cientifico = correo;
+    }
+
+    public String gettxtfamilia() {
+        return txtfamilia;
+    }
+
+    public void settxtfamilia(String id) {
+        this.txtfamilia = id;
+    }
+
+    public String gettxtextincion() {
+        return txtextincion;
+    }
+
+    public void settxtextincion(String password) {
+        this.txtextincion = password;
+    }
+
+    public String gettxtidentificacion() {
+        return txtidentificacion;
+    }
+
+    public void settxtidentificacion(String tel) {this.txtidentificacion = tel;}
+
+    public String gettxtespecie() {
+        return txtespecie;
+    }
+
+    public void settxtespecie(String tel) {
+        this.txtespecie = tel;
+    }
+
+    public String gettxtgenero() {
+        return txtgenero;
+    }
+
+    public void settxtgenero(String tel) {
+        this.txtgenero = tel;
+    }
+    public String gettxtnacimiento() {
+        return txtnacimiento;
+    }
+
+    public void settxtnacimiento(String tel) {
+        this.txtnacimiento = tel;
+    }
+    public String gettxtzoo() {
+        return txtzoo;
+    }
+
+    public void settxtzoo(String tel) {
+        this.txtzoo = tel;
+    }
+
+    public String gettxtpaisor() {
+        return txtpaisor;
+    }
+
+    public void settxtpaisor(String tel) {
+        this.txtpaisor = tel;
+    }
+
+    public String gettxtcontinente() {
+        return txtcontinente;
+    }
+
+    public void settxtcontinente(String tel) {
+        this.txtcontinente = tel;
+    }
+}
--- app/src/main/java/com/example/proyectofinal/moduloadministrador/animalesa/animalesAFragment.java
+++ app/src/main/java/com/example/proyectofinal/moduloadministrador/animalesa/animalesAFragment.java
@@ -1,20 +1,169 @@
 package com.example.proyectofinal.moduloadministrador.animalesa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.proyectofinal.ActivityRegistrar;
+import com.example.proyectofinal.Modelo.CRUDanimales;
+import com.example.proyectofinal.R;
 import com.example.proyectofinal.databinding.FragmentAnimalesaBinding;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class animalesAFragment extends Fragment {
 
     private FragmentAnimalesaBinding binding;
+    EditText txtNomVulgar,txtNomCientifico,txtFamilia,txtExtincion,txtIdentificacion,txtEspecie,txtGenero,txtNacimiento,txtZoo,txtPaisor,txtContinente;
+    Button btnCargarZoo;
+    public static List<CRUDanimales> lista = new List<CRUDanimales>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(@Nullable Object o) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Iterator<CRUDanimales> iterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @NonNull
+            @Override
+            public <T> T[] toArray(@NonNull T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(CRUDanimales cruDanimales) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(@Nullable Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(@NonNull Collection<? extends CRUDanimales> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, @NonNull Collection<? extends CRUDanimales> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public CRUDanimales get(int index) {
+                return null;
+            }
+
+            @Override
+            public CRUDanimales set(int index, CRUDanimales element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, CRUDanimales element) {
+
+            }
+
+            @Override
+            public CRUDanimales remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(@Nullable Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(@Nullable Object o) {
+                return 0;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<CRUDanimales> listIterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<CRUDanimales> listIterator(int index) {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public List<CRUDanimales> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,8 +172,125 @@ public class animalesAFragment extends Fragment {
 
         binding = FragmentAnimalesaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        final Button btnAgregar = (Button) root.findViewById(R.id.btnAgregar);
+        txtNomVulgar = root.findViewById(R.id.txtNomVulgar);
+        txtNomCientifico = root.findViewById(R.id.txtNomCientifico);
+        txtFamilia = root.findViewById(R.id.txtFamilia);
+        txtEspecie = root.findViewById(R.id.txtEspecie);
+        txtExtincion = root.findViewById(R.id.txtExistinción);
+        txtIdentificacion = root.findViewById(R.id.txtIdentificacion);
+        txtEspecie = root.findViewById(R.id.txtEspecie);
+        txtGenero = root.findViewById(R.id.txtGenero);
+        txtNacimiento = root.findViewById(R.id.txtNacimiento);
+        txtZoo = root.findViewById(R.id.txtZoo);
+        txtPaisor = root.findViewById(R.id.txtPaisOr);
+        txtContinente = root.findViewById(R.id.txtContinente);
+        btnAgregar.setOnClickListener(this::onClick);
 
         return root;
+    }
+
+    private void onClick(View v)
+    {
+        //validacion para los startActivitis
+        int id=v.getId();
+        if(id==R.id.btnAgregar)
+        {
+            //comprobacion de que cualquier campo no este vacio
+            String[] a = {txtNomVulgar.getText().toString(),txtNomCientifico.getText().toString()
+                    , txtFamilia.getText().toString(), txtEspecie.getText().toString()
+                    , txtExtincion.getText().toString() , txtIdentificacion.getText().toString()
+                    , txtNacimiento.getText().toString() , txtGenero.getText().toString()
+                    , txtZoo.getText().toString(), txtPaisor.getText().toString()
+                    , txtContinente.getText().toString()};
+            boolean vacio=false;
+            for (String aux:a)
+            {
+                if(aux.isEmpty())
+                {
+                    vacio=true;
+                    break;
+                }
+            }
+//controlando que los campos cumplan con los requisitos de la bd y no se pasen de los caracteres
+            if(vacio)
+            {
+                Toast.makeText(getContext(), "Los campos no deben estar vacios", Toast.LENGTH_SHORT).show();
+            }else
+            {
+                //llenando el objeto de Usuario enviandolo a la funcion insertar
+
+                CRUDanimales ob = new CRUDanimales();
+                ob.setnom_vulgar(txtNomVulgar.getText().toString().trim());
+                ob.setnom_cientifico(txtNomCientifico.getText().toString().trim());
+                ob.settxtfamilia(txtFamilia.getText().toString().trim());
+                ob.settxtespecie(txtEspecie.getText().toString().trim());
+                ob.settxtextincion(txtExtincion.getText().toString().trim());
+                ob.settxtidentificacion(txtIdentificacion.getText().toString().trim());
+                ob.settxtnacimiento(txtNacimiento.getText().toString().trim());
+                ob.settxtgenero(txtGenero.getText().toString().trim());
+                ob.settxtzoo(txtZoo.getText().toString().trim());
+                ob.settxtcontinente(txtContinente.getText().toString().trim());
+                insertarAnimal(ob);
+
+            }
+            //direccionando el boton volver al login
+        }
+
+    }
+    private void insertarAnimal(CRUDanimales ob)
+    {
+        // aqui se pone el link de la base de datos de la nube
+        StringRequest request = new StringRequest(Request.Method.POST, "https://compbdzoo.000webhostapp.com/insertarAnimal.php",
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+
+                        if(response.equalsIgnoreCase("Registro con éxito"))
+                        {
+                            Toast.makeText(getContext(), "Registro no fue éxitoso", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(getContext(), "El registro fue exitoso", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                //con map se envian los parametros por el link con el methodo post
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("nom_vulgar",ob.getnom_vulgar());
+                params.put("nom_cientifico",ob.getnom_cientifico());
+                params.put("txtfamilia",ob.gettxtfamilia());
+                params.put("txtextincion",ob.gettxtextincion());
+                params.put("txtidentificacion",ob.gettxtidentificacion());
+                params.put("txtespecie",ob.gettxtespecie());
+                params.put("txtgenero",ob.gettxtgenero());
+                params.put("txtnacimiento",ob.gettxtnacimiento());
+                params.put("txtzoo",ob.gettxtzoo());
+                params.put("txtpaisor",ob.gettxtpaisor());
+                params.put("txtcontinente",ob.gettxtcontinente());
+
+                return params;
+            }
+        };
+
+        //ejecutando la direccion ip dirigida a la base de datos
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(request);
     }
 
     @Override
